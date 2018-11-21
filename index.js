2b86b4aba5df4a5b2b6d0b530636ae284cdd7821
naaltunian <naaltunian@icloud.com>
2018-11-21 18:36:22 -0500

    ready app for deployment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require("./schema.js");
+const path = require("path");
 
 const cors = require('cors');
-
-// Allow cross-origin
 
 const app = express();
 app.use(cors())
@@ -13,6 +12,13 @@ app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+
+// deployment
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000
 
