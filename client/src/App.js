1fdd1aafe8213be65d93a23d5d92b1604d4c72a3
naaltunian <naaltunian@icloud.com>
2018-11-20 19:53:43 -0500

    add apollo packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import SpaceXLogo from './spacexLogo.png';
-import { Z_BLOCK } from 'zlib';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+})
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div>
-        <img src={SpaceXLogo} alt="SpaceX" style={{width: 300, display: "block", margin: "auto"}}></img>
+        <img src={SpaceXLogo} alt="SpaceX" style={{width: 300, display: "block", margin: "auto"}} />
       </div>
+      </ApolloProvider>
     );
   }
 }
