8e85c635ec22a15780dbae64c6787448823952b8
naaltunian <naaltunian@icloud.com>
2018-11-20 19:38:27 -0500

    add logo and basic styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import SpaceXLogo from './spacexLogo.png';
+import { Z_BLOCK } from 'zlib';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>SpaceX</h1>
+        <img src={SpaceXLogo} alt="SpaceX" style={{width: 300, display: "block", margin: "auto"}}></img>
       </div>
     );
   }
