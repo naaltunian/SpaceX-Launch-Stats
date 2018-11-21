a20266a7ea2c113ace2325359af189c2c6ca6eb3
naaltunian <naaltunian@icloud.com>
2018-11-21 16:22:54 -0500

    add launch info to cards and basic styling

--- client/src/Components/Launch.js
+++ client/src/Components/Launch.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Launch = (props) => {
     return(
-        <div className="card card-body mb-3">{props.flight_number}</div>
+        <div className="card card-body mb-3">
+            <div className="row">
+                <div className="col-md-9">
+                    <h4>Mission: { props.launch.mission_name }</h4>
+                    <p>{ props.launch.launch_date_local }</p>
+                </div>
+                <div className="col-md-3">
+                    <button className="btn btn-secondary">Launch Details</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
